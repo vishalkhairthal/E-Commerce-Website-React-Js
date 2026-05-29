9e86221742094db686ca401a6b36c9b723dc373a
vishalkhairthal <vg3361783@gmail.com>
2026-05-29 17:24:22 +0530

    initial commit

---
+++ frontend/src/assets/newcollections.js
@@ -0,0 +1,20 @@
+import p1_img from '../assets/8.jpg'
+import p5_img from '../assets/1.jpg'
+import p6_img from '../assets/2.jpg'
+import p7_img from '../assets/3.jpg'
+import p8_img from '../assets/4.jpg'
+import p9_img from '../assets/5.jpg'
+import p10_img from '../assets/6.jpg'
+import p11_img from '../assets/7.jpg'
+let new_collections = [
+     { id: 5, name: "T-Shirt", category: "men", image: p5_img, new_price: 45.0, old_price: 70.0 },
+        { id: 6, name: "Dress", category: "women", image: p6_img, new_price: 85.0, old_price: 130.0 },
+        { id: 7, name: "Hoodie", category: "men", image: p7_img, new_price: 95.0, old_price: 150.0 },
+        { id: 8, name: "Leggings", category: "women", image: p8_img, new_price: 55.0, old_price: 100.0 },
+        { id: 9, name: "Shirt", category: "men", image: p9_img, new_price: 70.0, old_price: 110.0 },
+        { id: 10, name: "Jumpsuit", category: "women", image: p10_img, new_price: 88.0, old_price: 135.0 },
+        { id: 11, name: "Sweater", category: "men", image: p11_img, new_price: 92.0, old_price: 145.0 },
+        { id: 12, name: "Gown", category: "women", image: p1_img, new_price: 99.0, old_price: 160.0 },
+]
+
+export default new_collections;
